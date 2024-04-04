6da97576df8119555214054d29837d85d1001917
msp3021 <131592979+msp3021@users.noreply.github.com>
2024-04-04 10:04:10 +0200

    js code

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+const createGallary = galleryItems
+  .map(
+    (galleryItems) =>
+      `<li class="gallery__item">
+   <a class="gallery__link" href="${galleryItems.original}">
+   <img class="gallery__image" src="${galleryItems.preview}" data-src="${galleryItems.original}" alt="${galleryItems.description}" />
+   </a>
+   </li>`
+  )
+  .join("");
+galleryContainer.insertAdjacentHTML("beforeend", createGallary);
+
+galleryContainer.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName === "IMG") {
+    const imageSrc = event.target.dataset.src;
+    const instance = basicLightbox.create(`
+        <img src="${imageSrc}" alt = "${event.target.alt}" width="800" height="600">
+      `);
+    instance.show();
+  }
+});
+
+
+
 
 console.log(galleryItems);
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+
+const galleryContainer = document.querySelector(".gallery");
+const createGallaryItems = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" data-src="${original}" alt="${description}" />
+      </a>
+    </li>`
+  )
+  .join("");
+galleryContainer.insertAdjacentHTML("beforeend", createGallaryItems);
+
+const galleryHandler = new SimpleLightbox("ul.gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+galleryHandler.on("show.simplelightbox");
 
 console.log(galleryItems);
